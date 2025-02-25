cd483118f97b7a8182ab334c4a6dbfc8dca52c47
Mirnes Glamočić <contact@mirnesglamocic.com>
2025-02-25 02:48:56 +0100

    Updated files and hosted on domain

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
-# PHP function rand
-This repository contains a simple single-serving site that randomly changes the background color of the page, text, font style, font size, font color, and text-align. All this was done with the help of the PHP function rand. Also, there is a built-in PHP function count that counts the elements of the array, so regardless of the number of elements in the array, they will always be displayed randomly every time the web page is opened or refreshed. With array_delete function color matching for text and background is prevented.
+# PHP-one page site
+This repository contains a simple single-serving web site that randomly changes one of two text that is please visit mirnes.glamocic.com and visit my page. Then, in the same time is changed the background color of the page, font style, font size, font color, and text-align. All this was done with the help of the PHP function rand. There is implemented a built-in PHP function count that counts the elements of the array, so regardless of the number of elements in the array, they will always be displayed randomly every time the web page is opened or refreshed. With array_delete function color matching for text and background is prevented.
 
-Of course, this example can be used to modify anything else using the rand function.
+Of course, this project can be used to modify anything else using the rand function.
 
 ---
 
-To check how it looks, click on the preview and then on the green run button.
+To check how it looks, click on the image or on the preview .
+
+[![mockup of personal website mirnesglamocic.com by web developer, UI/UX and web designer Mirnes Glamočić, Jajce, BiH](/mockup.webp)](https://php-one-page-site.mirnesglamocic.com)
 ##
-[PREVIEW](https://replit.com/@MirnesGlamocic/PHP-function-rand?v=1)
+[PREVIEW](https://php-one-page-site.mirnesglamocic.com)
--- index.php
+++ index.php
@@ -13,7 +13,7 @@ function array_delete($value, $array) {
   $text_color_count = count($text_colors);
   $text_color = $text_colors[rand(0, $text_color_count -1)];
   
-  $font_size = (80 + (rand(0,15) * 5));
+  $font_size = (80 + (rand(0,15) * 3));
   
   $aligns = array('center', 'left', 'right', 'justify');
   $text_align = $aligns[rand(0,3)];
@@ -21,7 +21,7 @@ function array_delete($value, $array) {
   $styles = array('bold', 'italic', 'underline', 'uppercase');
   $style = $styles[rand(0,3)];
 
-  $pages = array('Please visit mirnesglamocic.com', 'Welcome to my page!');
+  $pages = array('Please visit <a href="https://mirnesglamocic.com" target="_blank">mirnesglamocic.com</a>', 'Welcome to <a href="https://mirnesglamocic.com" target="_blank">my page</a>!');
   $page = $pages[rand(0,1)]
 ?>
 
@@ -30,6 +30,37 @@ function array_delete($value, $array) {
   <head>
     <meta charset="UTF-8">
     <title>Welcome to my page!</title>
+    <meta name="description" content="A Shoutbox that allows people to quickly leave messages on the website, without any form of user registration">
+    <meta name="author" content="Mirnes Glamočić">
+    <!-- Facebook & Viber -->
+    <meta name="title" property="og:title" content="Shoutbox">
+
+    <!-- Facebook, Viber, LinkedIn, Whatsapp & KakaoTalk(meta tags) -->
+    <meta property="og:url" content="https://php-one-page-site.mirnesglamocic.com/">
+    <meta property="og:type" content="website">
+    <meta property="og:image" content="https://php-one-page-site.mirnesglamocic.com/mockup-og.jpg">
+    <meta property="og:image:type" content="image/jpg">
+    <meta property="og:image:width" content="1200">
+    <meta property="og:image:height" content="630">
+    <meta property="og:image:alt" content="PHP-One page site">
+    <meta property="og:site_name" content="PHP-One page site">
+    <meta property="og:locale" content="en_us">
+    <meta property="fb:app_id" content="1007101964814604">
+
+    <!-- LinkedIn, Whatsapp & KakaoTalk(meta tag) -->
+    <meta property="og:title" content="PHP-One page site">
+
+    <!-- Twitter Meta Tags -->
+    <meta name="twitter:card" content="summary">
+    <meta name="twitter:title" content="PHP-One page site">
+    <meta name="twitter:description" content="A simple single-serving web site that randomly changes text, background color, font style, font size, font color, and text-align.">
+    <meta name="twitter:image:src" content="https://php-one-page-site.mirnesglamocic.com/mockup-og.webp">
+    <meta name="twitter:image:alt" content="PHP-One page site">
+
+    <!-- Google / Search Engine Tags -->
+    <meta itemprop="name" content="https://php-one-page-site.mirnesglamocic.com/">
+    <meta itemprop="description" content="A simple single-serving web site that randomly changes text, background color, font style, font size, font color, and text-align.">
+    <meta itemprop="image" content="PHP-One page site">
   	<style type="text/css">
     body {
     	margin: 0;
