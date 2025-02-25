eb31ee2ff16866c861b9bcee0f4f7389f5b510c5
Mirnes Glamočić <contact@mirnesglamocic.com>
2025-02-25 04:18:00 +0100

    Added favicon

--- index.php
+++ index.php
@@ -61,6 +61,11 @@ function array_delete($value, $array) {
     <meta itemprop="name" content="https://php-one-page-site.mirnesglamocic.com/">
     <meta itemprop="description" content="A simple single-serving web site that randomly changes text, background color, font style, font size, font color, and text-align.">
     <meta itemprop="image" content="PHP-One page site">
+    <!-- favicon -->
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
   	<style type="text/css">
     body {
     	margin: 0;
